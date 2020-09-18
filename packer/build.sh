ff717a927255d330cf77b3525ebd6644d423fdae
Nightmayr <umayr23@gmail.com>
2020-09-18 22:26:56 +0100

    removing obsolete script

--- packer/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-ARTIFACT=`packer build -machine-readable jenkins-packer.json |awk -F, '$0 ~/artifact,0,id/ {print $6}'`
-jenkins_image_ID=`echo $ARTIFACT | cut -d ':' -f2`
-echo 'variable "jenkins_image_ID" { default = "'${jenkins_image_ID}'" }' > ../terraform/jenkins_imagevar.tf
-#terraform init
-#terraform apply
